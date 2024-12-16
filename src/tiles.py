266e9ca6cc4f99c8a1d639e602f7dd47534ae02a
kamyar <52869955+kamyarscode@users.noreply.github.com>
2024-12-16 15:41:25 -0700

    change view to 1000 tiles

--- src/tiles.py
+++ src/tiles.py
@@ -244,7 +244,7 @@ image_path = "img_src/car.jpg"
 input_image = open_image(image_path).convert("RGB")
 
 # Apply Voronoi mosaic with region merging
-output_image = voronoi_with_region_merging(input_image, num_points=500, line_thickness=1, color_threshold=60)
+output_image = voronoi_with_region_merging(input_image, num_points=1000, line_thickness=1, color_threshold=60)
 
 # Save or display the output
 output_image.show()
